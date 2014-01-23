dce17bffc8ffb79fc908142bff24920c6645381a
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-01-23 15:36:14 +0100

    Update AllowInternetArchiver.i18n.php
    Added German translation

--- AllowInternetArchiver.i18n.php
+++ AllowInternetArchiver.i18n.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * Internationalisation for AllowInternetArchiver
+ * Internationalisation file for the AllowInternetArchiver extension
  *
  * @file
  * @ingroup Extensions
  */
+
 $messages = array();
 
 /** English
@@ -12,4 +13,11 @@ $messages = array();
  */
 $messages['en'] = array(
 	'allowinternetarchiver-desc' => 'Allows the Internet Archiver to index the wiki'
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'allowinternetarchiver-desc' => 'Erlaubt die Indexierung des Wikis durch das „Internet Archive“'
 );
